15726b2e9531f942aefdccdb4ef740513820422d
Muhsina Kamal <muhsina_k@hotmail.co.uk>
2024-10-23 11:47:41 +0100

    feat: add navlinks with simple styling and integrate into navbar

--- src/components/ResponsiveNavbar.jsx
+++ src/components/ResponsiveNavbar.jsx
@@ -1,9 +1,11 @@
+import NavLinks from "./NavLinks";
 import "./ResponsiveNavbar.css";
 
 export default function ResponsiveNavbar() {
     return (
         <header className="header">
             <nav aria-label="The Maths Club" className="container nav">
+                <NavLinks />
                 <a href="/" className="nav__logo" aria-label="Go to the THEMATHSCLUB.COM homepage">
                     <span>THE</span>
                     <span>MATHS</span>
