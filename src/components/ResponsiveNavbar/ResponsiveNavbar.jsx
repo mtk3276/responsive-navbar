a9556484900effaaab2178243f689bcf6cf34267
Muhsina Kamal <muhsina_k@hotmail.co.uk>
2024-10-23 12:52:51 +0100

    feat: add media query and styles for responsiveness

--- src/components/ResponsiveNavbar/ResponsiveNavbar.jsx
+++ src/components/ResponsiveNavbar/ResponsiveNavbar.jsx
@@ -1,11 +1,14 @@
+import { useMediaQuery } from "react-responsive";
 import NavLinks from "../NavLinks/NavLinks";
 import "./ResponsiveNavbar.css";
 
 export default function ResponsiveNavbar() {
+    const isMobile = useMediaQuery({ maxWidth: "1150px" });
+
     return (
         <header className="header">
-            <nav aria-label="The Maths Club" className="container nav">
-                <NavLinks />
+            <nav aria-label="The Maths Club" className={`container nav ${isMobile ? "nav-mobile" : "nav-web"}`}>
+                <NavLinks isMobile={isMobile}/>
                 <a href="/" className="nav__logo" aria-label="Go to the THEMATHSCLUB.COM homepage">
                     <span>THE</span>
                     <span>MATHS</span>
