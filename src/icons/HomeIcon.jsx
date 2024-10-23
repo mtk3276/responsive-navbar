cba0357e1d9b450845d48e2451c4c9c3c8bd7b1b
Muhsina Kamal <muhsina_k@hotmail.co.uk>
2024-10-23 13:35:06 +0100

    feat: add svg icons to src/icons directory

---
+++ src/icons/HomeIcon.jsx
@@ -0,0 +1,12 @@
+import StyledSvgIcon from "./StyledSvgIcon";
+
+export default function HomeIcon({ fill = "white", ...props }) {
+    return (
+        <StyledSvgIcon fill={fill} {...props}>
+            <path d="M425,300H75c-13.807,0-25,11.193-25,25v150c0,13.807,11.193,25,25,25h350c13.807,0,25-11.193,25-25V325
+            C450,311.193,438.807,300,425,300z"/>
+            <path d="M491.856,204.226L266.509,6.063c-9.44-8.301-23.577-8.301-33.017,0L8.144,204.227C-9.168,219.45,1.599,248,24.653,248
+            h450.694C498.401,248,509.168,219.45,491.856,204.226z"/>
+        </StyledSvgIcon>
+    )   
+}
---
+++ src/icons/StyledSvgIcon.jsx
@@ -0,0 +1,28 @@
+import { styled } from "@mui/material";
+
+export default function StyledSvgIcon({ fill = "white", children, ...props }) {
+
+    const StyledSvg = styled('svg')({
+        width: '100px',
+        height: 'auto',
+        fill: fill,
+        cursor: 'pointer',
+        transition: 'transform 0.3s ease',
+        '&:hover': {
+        transform: 'scale(1.1)', // Add hover effects if needed
+        },
+    });
+
+    return (
+        <StyledSvg
+            {...props}
+            version="1.1" 
+            xmlns="http://www.w3.org/2000/svg" 
+            xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="0 0 500 500" 
+            style={{ enableBackground: 'new 0 0 500 500' }} 
+            xmlSpace="preserve">
+                {children}
+        </StyledSvg>
+    )
+}
